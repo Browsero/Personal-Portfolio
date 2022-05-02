0381c4c7dcf1346c4c7818f792db24619ef0a0bf
Browsero <hubertmadej02@gmail.com>
2022-05-02 21:46:05 +0200

    New Button Style, Fixed Navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
   const age = year - 2002;
   return (
     <>
-      <header className="mt-4 py-16 px-4 mx-auto min-h-screen h-96 md:flex md:items-center" data-aos='slide-up'>
+      <header className="mt-16 py-24 px-4 mx-auto min-h-screen h-96 md:flex md:items-center" data-aos='slide-up'>
         <div className="mx-auto flex flex-col justify-center items-center text-center space-y-2 max-w-sm sm:max-w-2xl md:max-w-4xl lg:max-w-7xl xl:max-w-9xl">
           <div>
             <h2 className="font-medium text-lg sm:text-xl md:text-5xl text-gray-600">
@@ -14,7 +14,7 @@ export default function Header() {
           </div>
           <div>
             <h1 className="text-4xl font-bold sm:text-5xl md:text-6xl xl:text-7xl">
-              I'm a computer science student and begginer Web Developer.
+              I'm a computer science student and trainee Web Developer.
             </h1>
           </div>
           <div className="md:max-w-5xl">
@@ -25,7 +25,7 @@ export default function Header() {
             </p>
           </div>
           <div>
-            <button className="text-white bg-gray-800 flex flex-wrap p-4 mt-8 transition delay-150 md:hover:scale-110 duration-300">
+            <button className="text-white bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 flex flex-wrap p-4 mt-8 transition delay-150 md:hover:scale-110 duration-300">
               <a href="./#projects">Check out my projects!</a>
             </button>
           </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,79 +16,81 @@ export default function Navbar() {
   const [menuClicked, setMenuClicked] = useState(false);
   return (
     <>
-      <nav className="shadow-lg max-w-sm mx-auto rounded-lg border bg-white border-gray-200 mt-8 flex justify-around sm:max-w-xl md:border-0 md:shadow-none md:max-w-none md:rounded-none md:mt-4">
-        <div className="hidden md:flex space-x-4 text-2xl">
-          <ul className="flex space-x-8">
-            <li>
-              <a href="https://twitter.com/HubertMadej01">
-                <AiOutlineTwitter className="text-gray-700 cursor-pointer transition md:hover:scale-110" />
-              </a>
-            </li>
-            <li>
-              <a href="https://www.linkedin.com/in/hubert-madej-774801227/">
-                <AiOutlineLinkedin className="text-gray-700 cursor-pointer transition md:hover:scale-110" />
-              </a>
-            </li>
-            <li>
-              <a href="https://dev.to/browser">
-                <SiDevdotto className="text-gray-700 cursor-pointer transition md:hover:scale-110" />
-              </a>
-            </li>
-            <li>
-              <a href="https://github.com/Browsero">
-                <AiFillGithub className="text-gray-700 cursor-pointer transition md:hover:scale-110" />
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="p-2 md:p-0">
-          <ul className="flex flex-row space-x-8">
-            <li className="flex flex-col items-center">
-              <AiOutlineHome className="text-4xl md:hidden" />
-              <a className=" text-gray-700 font-medium" href="./#header">
-                Home
-              </a>
-            </li>
-            <li className="flex flex-col items-center">
-              <HiOutlineBookOpen className="text-4xl md:hidden" />
-              <a className="text-gray-700 font-medium" href="./#projects">
-                Work
-              </a>
-            </li>
-            <li className="flex flex-col items-center">
-              <BiLinkExternal className="text-4xl md:hidden" />
-              <a className=" text-gray-700 font-medium" href="./#contact">
-                Contact
-              </a>
-            </li>
-            <li className="flex flex-col items-center">
-              {!menuClicked ? (
-                <GiHamburgerMenu
-                  onClick={() => {
-                    setMenuClicked(true);
-                  }}
-                  className="text-4xl md:hidden"
-                />
-              ) : (
-                <AiOutlineClose
-                  onClick={() => {
-                    setMenuClicked(false);
-                  }}
-                  className="text-4xl md:hidden"
-                />
-              )}
-              <a className="text-gray-700 font-medium md:hidden" href="#">
-                {!menuClicked ? "Menu" : "Close"}
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <div className="fixed w-full top-0 z-10 pl-2 pr-2 md:static">
+        <nav className="shadow-lg mx-auto rounded-lg border bg-white border-gray-200 mt-8 flex justify-around sm:max-w-xl md:border-0 md:shadow-none md:max-w-none md:rounded-none md:mt-4">
+          <div className="hidden md:flex space-x-4 text-2xl">
+            <ul className="flex space-x-8">
+              <li>
+                <a href="https://twitter.com/HubertMadej01">
+                  <AiOutlineTwitter className="text-gray-700 cursor-pointer transition md:hover:scale-110" />
+                </a>
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/in/hubert-madej-774801227/">
+                  <AiOutlineLinkedin className="text-gray-700 cursor-pointer transition md:hover:scale-110" />
+                </a>
+              </li>
+              <li>
+                <a href="https://dev.to/browser">
+                  <SiDevdotto className="text-gray-700 cursor-pointer transition md:hover:scale-110" />
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/Browsero">
+                  <AiFillGithub className="text-gray-700 cursor-pointer transition md:hover:scale-110" />
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="p-2 md:p-0">
+            <ul className="flex flex-row space-x-8">
+              <li className="flex flex-col items-center">
+                <AiOutlineHome className="text-4xl md:hidden" />
+                <a className=" text-gray-700 font-medium" href="./#header">
+                  Home
+                </a>
+              </li>
+              <li className="flex flex-col items-center">
+                <HiOutlineBookOpen className="text-4xl md:hidden" />
+                <a className="text-gray-700 font-medium" href="./#projects">
+                  Work
+                </a>
+              </li>
+              <li className="flex flex-col items-center">
+                <BiLinkExternal className="text-4xl md:hidden" />
+                <a className=" text-gray-700 font-medium" href="./#contact">
+                  Contact
+                </a>
+              </li>
+              <li className="flex flex-col items-center">
+                {!menuClicked ? (
+                  <GiHamburgerMenu
+                    onClick={() => {
+                      setMenuClicked(true);
+                    }}
+                    className="text-4xl md:hidden"
+                  />
+                ) : (
+                  <AiOutlineClose
+                    onClick={() => {
+                      setMenuClicked(false);
+                    }}
+                    className="text-4xl md:hidden"
+                  />
+                )}
+                <a className="text-gray-700 font-medium md:hidden" href="#">
+                  {!menuClicked ? "Menu" : "Close"}
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
 
       {menuClicked && (
-        <div
-          className="shadow-lg max-w-sm mx-auto rounded-lg border border-gray-200 mt-8 flex sm:max-w-xl md:shadow-none md:max-w-none md:rounded-none md:mt-4 md:hidden"
-          data-aos="slide-up"
+        <div className="fixed z-10 mt-16 w-full p-4"  data-aos="slide-up">
+          <div
+          className="shadow-lg mx-auto rounded-lg border bg-white border-gray-200 flex sm:max-w-xl md:hidden"
         >
           <ul className="p-4">
             <li>
@@ -118,6 +120,7 @@ export default function Navbar() {
               </a>
             </li>
           </ul>
+        </div>
         </div>
       )}
     </>
